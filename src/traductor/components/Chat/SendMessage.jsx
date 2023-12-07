fad5ee6e17805d753eb5fdd1ccff4c847eedd162
datou <datou@gmail.com>
2023-12-07 04:23:18 -0400

    audio

--- src/traductor/components/Chat/SendMessage.jsx
+++ src/traductor/components/Chat/SendMessage.jsx
@@ -1,37 +1,126 @@
-import { Send } from '@mui/icons-material'
-import React, { useState } from 'react'
+import { Mic, Send, Stop } from "@mui/icons-material";
+import React, { useRef, useState } from "react";
+
+const mimeType = "audio/webm";
 
 export const SendMessage = () => {
-  const [mensaje, setMensaje] = useState('');
+  const [mensaje, setMensaje] = useState("");
+  const [recording, setRecording] = useState(false);
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [audio, setAudio] = useState(null);
+  const mediaRecorder = useRef(null);
+
+  const [permission, setPermission] = useState(false);
+  const [stream, setStream] = useState(null);
+  let st;
+
+  const getMicrophonePermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        setPermission(true);
+        setStream(streamData);
+        st = streamData;
+        console.log(streamData);
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
+
+  const toggleRecording = () => {
+    if (!recording) {
+      if (!permission) getMicrophonePermission();
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  };
+
+  const startRecording = () => {
+    setRecording(true);
+    const streamToUse = stream || st;
+
+    if (streamToUse) {
+      const media = new MediaRecorder(streamToUse, { type: mimeType });
+      mediaRecorder.current = media;
+      let localAudioChunks = [];
+
+      mediaRecorder.current.ondataavailable = (event) => {
+        console.log(event);
+        if (typeof event.data === "undefined" || event.data.size === 0) return;
+        localAudioChunks.push(event.data);
+      };
+
+      mediaRecorder.current.onstop = () => {
+        const audioBlob = new Blob(localAudioChunks, { type: mimeType });
+        // console.log(audioBlob);
+        const audioUrl = URL.createObjectURL(audioBlob);
+        setAudio(audioUrl);
+        setAudioChunks([]);
+      };
+
+      mediaRecorder.current.start();
+    } else {
+      // Handle the case when the stream is not available
+      console.error("Stream not available");
+      setRecording(false);
+    }
+  };
+
+  const stopRecording = () => {
+    setRecording(false);
+    if (mediaRecorder.current) {
+      mediaRecorder.current.stop();
+    }
+  };
 
   const handleInputChange = (event) => {
     setMensaje(event.target.value);
   };
 
   const handleSend = () => {
-    console.log('Mensaje enviado:', mensaje);
+    console.log("Mensaje enviado:", mensaje);
     // Aquí puedes realizar cualquier lógica adicional con el mensaje
 
     // Limpiar el input después de enviar el mensaje
-    setMensaje('');
+    setMensaje("");
   };
 
   return (
     <div className="type_msg row">
-        <div className="input_msg_write col-sm-9">
-            <input
-              type="text" 
-              className="write_msg" 
-              placeholder="Mensaje..."
-              value={mensaje}
-              onChange={handleInputChange}
-            />
-        </div>
-        <div className="col-sm-3 text-center">
-            <button className="msg_send_btn mt-3" type="submit" onClick={handleSend}>
-                <Send/>
-            </button>
-        </div>
-    </div>  
-  )
-}
+      <div className="input_msg_write col-sm-9">
+        <input
+          type="text"
+          className="write_msg"
+          placeholder="Mensaje..."
+          value={mensaje}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="col-sm-3 text-center">
+        {audio ? <audio src={audio} controls></audio> : null}
+        <button
+          type="button"
+          style={{ background: recording ? "red" : "" }}
+          onClick={toggleRecording}
+        >
+          {recording ? <Stop /> : <Mic />}
+        </button>
+
+        <button
+          className="msg_send_btn mt-3"
+          type="button"
+          onClick={handleSend}
+        >
+          <Send />
+        </button>
+      </div>
+    </div>
+  );
+};
